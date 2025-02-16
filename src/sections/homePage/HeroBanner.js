2a99f9f0dd9de9e1fa823736f8e38389064e12dc
Abdelrahman Essam <64382556+3BDO-debug@users.noreply.github.com>
2025-02-16 06:18:13 +0200

    fixed minors

--- src/sections/homePage/HeroBanner.js
+++ src/sections/homePage/HeroBanner.js
@@ -21,74 +21,7 @@ function HeroBanner() {
       }}
     >
       {/* Clouds */}
-      {/* <Box
-        component="img"
-        src="/cloud.svg"
-        sx={{
-          width: 300,
-          position: "absolute",
-          left: "-5%",
-          zIndex: 3,
-          top: "2%",
-        }}
-      />
-      <Box
-        component="img"
-        src="/cloud.svg"
-        sx={{
-          width: { xs: 200, md: 300 },
-          position: "absolute",
-          left: { xs: "50%", md: "30%" },
-          zIndex: 3,
-          bottom: 0,
-        }}
-      />
-      <Box
-        component="img"
-        src="/cloud.svg"
-        sx={{
-          width: { xs: 200, md: 300 },
-          position: "absolute",
-          left: { xs: "50%", md: "30%" },
-          zIndex: 3,
-          top: "20%",
-        }}
-      /> */}
-      {/* Hero Content */}
-      {/* <Box
-        sx={{
-          position: "absolute",
-          height: "100%",
-          bgcolor: "#000000",
-          width: { md: "40%", xs: "100%" },
-          zIndex: 2,
-          filter: "opacity(0.7)",
-          px: 3,
-          py: 6,
-        }}
-      >
-        <Stack
-          sx={{ height: "100%", zIndex: 10, mt: 2 }}
-          justifyContent="center"
-        >
-          <Typography sx={{ zIndex: 4 }} variant="h1" color="grey.100">
-            Your Ultimate Online Skincare destination.
-          </Typography>
-          <Typography color="grey.100" variant="body2">
-            It is a long established fact that a reader will be distracted by
-            the readable content of a page when looking at its layout. The point
-          </Typography>
-          <Box sx={{ flex: 0.3 }} />
-          <Box>
-            <Button variant="contained" size="large">
-              Shop Now
-            </Button>
-          </Box>
-          <Typography sx={{ mt: 3 }} variant="subtitle1" color="grey.200">
-            It is a long established fact that a reader will be distracted{" "}
-          </Typography>
-        </Stack>
-      </Box> */}
+    
       <Box
         sx={{
           position: "absolute",
